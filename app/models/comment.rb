68abdf39b77afe03d2ffafbe6e7ea546b9dcb25f
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-25 17:43:45 -0500

    Include the method that updates the comments counter for a post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  private
+
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
