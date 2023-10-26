68e79bb6441a791d1dff005047ee8e9e019128ae
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-26 17:45:17 -0500

    Test if the comments_counter is valid if less than zero

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -18,5 +18,10 @@ RSpec.describe Post, type: :model do
     post = Post.new(user: user, title: 'a' * 255, comments_counter: 0, likes_counter: 0)
     expect(post).to_not be_valid
   end
-   
+  
+  it 'is invalid with a comments_counter less than zero' do
+    user = User.create(name: 'Tom', posts_counter: 0)
+    post = Post.new(user: user, title: 'Test Post', comments_counter: -1, likes_counter: 0)
+    expect(post).to_not be_valid
+  end
 end
