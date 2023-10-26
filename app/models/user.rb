adf830f2defe28ba2550a71b84eeb483af8a5fd7
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-26 09:24:59 -0500

    Add a validation to the user name not to be blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :name, presence: true
+
   def three_most_recent_post
     posts.order(created_at: :desc).limit(3)
   end
