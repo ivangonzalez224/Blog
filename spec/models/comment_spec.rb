ff2dcead963fe1215eee81eaa6302bb95f1d9eb2
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-26 18:24:15 -0500

    Test the update_comments_counter for a comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,4 +7,24 @@ RSpec.describe Comment, type: :model do
     comment = Comment.new(text: 'Test comment', post: post, user: user)
     expect(comment).to be_valid
   end
-end  
+
+  it 'is invalid without a post' do
+    user = User.create(name: 'Jim', posts_counter: 0)
+    Post.create(user: user, title: 'Post for testing', comments_counter: 0, likes_counter: 0)
+    comment = Comment.new(user: user)
+    expect(comment).to_not be_valid
+  end
+
+  describe '#update_comments_counter' do
+    it 'updates comments_counter when a comment is saved' do
+      user = User.create(name: 'Jim')
+      post = Post.create(user: user, title: 'Test Post', comments_counter: 0, likes_counter: 0)
+
+      comment = Comment.new(text: 'Comment for test', post: post, user: user)
+      expect do
+        comment.save
+        post.reload
+      end.to change(post, :comments_counter).by(1)
+    end
+  end
+end
