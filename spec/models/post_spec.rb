1e7f2bae65b2a91a7b96b6d4ed9bb59f51d89974
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-26 17:50:35 -0500

    Test if the update_user_post_counter is working correctly

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -30,4 +30,16 @@ RSpec.describe Post, type: :model do
     post = Post.new(user: user, title: 'Post for testing', comments_counter: 0, likes_counter: 1.5)
     expect(post).to_not be_valid
   end
+
+  describe '#update_user_post_counter' do
+    it 'increments the user posts_counter when a post is saved' do
+      user = User.create(name: 'Ted', posts_counter: 0)
+      post = Post.new(user: user, title: 'Test Post', comments_counter: 0, likes_counter: 0)
+
+      expect do
+        post.save
+        user.reload
+      end.to change(user, :posts_counter).by(1)
+    end
+  end
 end
