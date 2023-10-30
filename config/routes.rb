2eabea33c0693e496bf83e674fa6ef1665d2bbe5
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-30 09:42:51 -0500

    Update the routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :users, only: [:index, :show]
-  resources :posts, only: [:index, :show]
+  
+  get '/users/:user_id/posts', to: 'posts#index', as: :user_posts
+  get '/users/:user_id/posts/:id', to: 'posts#show', as: :user_post
 end
