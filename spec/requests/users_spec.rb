1957b067135254df7fe1ca3b2a08d7295ee23148
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-31 11:20:27 -0500

    Add a test for the index action of the UsersController

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
+RSpec.describe 'UsersControllers', type: :request do
   describe 'GET /users' do
     it 'returns a successful response' do
       get '/users'
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET #index' do
+    it 'returns a successful response for index action' do
+      get users_path
+      expect(response).to be_successful
     end
   end
 end
