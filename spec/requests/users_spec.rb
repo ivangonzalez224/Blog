a87a0532aa86d5481595357aec0bb026a73d48f8
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-31 10:57:02 -0500

    Test if the users path have a successful status response

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,1 +1,10 @@
-   
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
+    it 'returns a successful response' do
+      get '/users'
+      expect(response).to have_http_status(200)
+    end
+  end
+end
