6f1865ffb2a6d44f6191ccca49baed39baa8f4bd
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-31 16:16:27 -0500

    Test if the index action in the post controller returns a succesful status response

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'PostsController', type: :request do
+  describe 'GET #index' do
+    let(:user) { User.create(name: 'Tim', photo: 'Test photo', bio: 'Test bio') }
+
+    it 'returns a successful response' do
+      get user_posts_path(user)
+      expect(response).to have_http_status(200)
+    end
+  end
+end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'UsersControllers', type: :request do
-  describe 'GET /users' do
+  describe 'GET /users/:user_id/posts' do
     it 'returns a successful response' do
       get '/users'
       expect(response).to have_http_status(200)
