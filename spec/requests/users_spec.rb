1f6bf63c71ae1597b52c0095b48a6ea66ca893b0
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-31 13:35:36 -0500

    Test if the status response fail with an incorrect path for users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe 'UsersControllers', type: :request do
     end
   end
 
+  describe 'GET /users' do
+    it 'returns a successful response' do
+      get '/failed_path'
+      expect(response).to have_http_status(404)
+    end
+  end
+
   describe 'GET #index' do
     it 'returns a successful response for index action' do
       get users_path
