6a964faee0869ff865efbfd17f8eb8a374f512a7
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-02 13:27:29 -0500

    Add a route for a new comment post form

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-  end
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:new, :create]
+    end
+  end  
 end
