d7650f16ff3a70847d5173e337f23fad8951f107
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-11-06 17:10:48 -0500

    Test if the user show page displays the correct number of user posts

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -3,7 +3,17 @@ require 'rails_helper'
 RSpec.describe 'User Show Page', type: :feature do
   it 'displays the correct user profile image' do
     user = User.create(name: 'Tom', bio: 'Gamer', photo: 'https://i.blogs.es/d4590b/screenshot_113/1366_2000.jpeg')
+
     visit user_path(user)
     expect(page).to have_css("img[src*='#{user.photo}']")
+  end
+
+  it 'displays the correct number of user posts' do
+    user = User.create(name: 'Jim', bio: 'Student', photo: 'https://i.blogs.es/d4590b/screenshot_113/1366_2000.jpeg')
+    Post.create(title: 'Post title for testing', text: 'Post Text for testing', user: user)
+
+    visit user_path(user)
+
+    expect(page).to have_content('Number of posts: 1')
   end
 end
