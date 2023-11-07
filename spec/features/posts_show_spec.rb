cb61e6deda2b44867732378723921d863f8e2cb7
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-11-07 11:22:00 -0300

    Add test display post title

---
+++ spec/features/posts_show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+require 'capybara/rails'
+
+RSpec.describe 'User Post Show Page', type: feature do
+  let(:user) { User.create(name: 'Sam', bio: 'User Bio', photo: 'https://i.blogs.es/d4590b/screenshot_113/1366_2000.jpeg') }
+  let(:post) { Post.create(title: 'Post Title for testing', text: 'Post Text for testing', user: user) }
+
+  it 'displays the post title' do
+    Capybara.visit Rails.application.routes.url_helpers.user_post_path(user, post)
+    page = Capybara.page
+    expect(page).to have_content('Post Title for testing')
+  end
+
+end
