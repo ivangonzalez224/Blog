f94ba10605d70af8490569f051ad5996ac4b5293
Javier Aybar <javi_aybar@hotmail.com>
2023-11-09 09:20:53 -0300

    Define abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,15 @@
 class Ability
   include CanCan::Ability
+  
+  def initialize(user)
+    if user.is? :admin
+      can :manage, :all
+    else
+      can :read, :all
+      can :create, Post
+      can :create, Comment
+      can :delete, Post, author_id: user.id
+      can :delete, Comment, user_id: user.id
+    end
+  end
 end
