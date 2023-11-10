edd377c80226b66a8c121aa33cc7f6cba8bafd99
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-11-10 17:18:44 -0300

    Fix linters

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::SessionsController < ApplicationController
-    skip_before_action :verify_authenticity_token, only: [:create], if: -> { request.format.json? }
-    skip_before_action :authenticate_user!, only: [:create]
+  skip_before_action :verify_authenticity_token, only: [:create], if: -> { request.format.json? }
+  skip_before_action :authenticate_user!, only: [:create]
   def create
     @user = User.find_by(email: session_params[:email])
     if @user&.valid_password?(session_params[:password])
@@ -11,11 +11,14 @@ class Api::SessionsController < ApplicationController
       render json: { error: 'You entered an invalid email or password' }, status: :unauthorized
     end
   end
+
   private
+
   def generate_token(logged_in_user)
     payload = { user_id: logged_in_user.id, exp: 2.hours.from_now.to_i }
     JWT.encode(payload, Rails.application.secret_key_base)
   end
+
   def session_params
     params.permit(:email, :password)
   end
