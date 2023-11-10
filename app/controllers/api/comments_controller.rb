f7c0e350f894579795ebbb41795a7e233e80b7e5
kambereBr <brunokambere@gmail.com>
2023-11-10 22:15:50 +0100

    Create the index action for the comments controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,10 @@
 class Api::CommentsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create], if: -> { request.format.json? }
+  before_action :authenticate_user!, only: [:create]
+
+  def index
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments
+    render json: @comments
+  end
 end
