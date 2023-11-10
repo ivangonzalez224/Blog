970d9d06802222a5606ff45c2dca3210a7b34350
kambereBr <brunokambere@gmail.com>
2023-11-10 22:17:54 +0100

    Add the create action for the comments controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -7,4 +7,21 @@ class Api::CommentsController < ApplicationController
     @comments = @post.comments
     render json: @comments
   end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @user = logged_in_user
+    @new_user_comment = @post.comments.new(comment_params.merge(user_id: @user.id))
+    if @new_user_comment.save
+      render json: { success: 'Comment succesfully added!' }
+    else
+      render json: { error: @new_user_comment.errors.full_messages.join(', ') }
+    end
+  end
+
+  private
+
+  def comment_params
+    params.permit(:text)
+  end
 end
